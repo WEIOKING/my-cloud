bb4f76a17b6adfa37b04d6a1a2f7771e3575dea6
WEIOKING <624674746@qq.com>
2019-04-16 17:58:06 +0800

    add config-server add twoSum class

--- algorithm-server/src/main/java/cn/ply/cloud/algorithmsvr/leetcode/TwoSum.java
+++ algorithm-server/src/main/java/cn/ply/cloud/algorithmsvr/leetcode/TwoSum.java
@@ -1,11 +1,54 @@
 package cn.ply.cloud.algorithmsvr.leetcode;
 
+import java.util.HashMap;
+
 /**
  * @Author:ply
- * @Description:
+ * @Description: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
  * @Date: created in 2019/4/15
  * @Modified By:
  */
 public class TwoSum {
+    /**
+     * 暴力迭代法求解
+     *
+     * @param numList
+     * @param target
+     * @return
+     */
+    public static int[] violence(int[] numList, int target) {
+        for (int i = 0, l = numList.length; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                if (numList[i] + numList[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("没有找到符合要求的数！");
+    }
 
+
+    /**
+     * 一遍hash法
+     *
+     * @param numList
+     * @param target
+     * @return
+     */
+    public static int[] hash(int[] numList, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>(numList.length);
+        for (int i = 0, l = numList.length; i < l; i++) {
+            Integer integer = hashMap.get(target - numList[i]);
+            if (integer != null) {
+                return new int[]{integer, i};
+            }
+            hashMap.put(numList[i], i);
+        }
+        throw new IllegalArgumentException("没有找到符合要求的数！");
+    }
 }
---
+++ config-server/src/main/java/cn/ply/cloud/configsvr/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package cn.ply.cloud.configsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author:ply
+ * @Description:
+ * @Date: created in 2019/4/16
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
