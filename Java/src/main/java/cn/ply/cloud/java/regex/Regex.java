dfc2d33372444205fdb422cbe81c55a150443992
WEIOKING <624674746@qq.com>
2019-11-26 18:04:12 +0800

    Regex

---
+++ Java/src/main/java/cn/ply/cloud/java/regex/Regex.java
@@ -0,0 +1,17 @@
+package cn.ply.cloud.java.regex;
+
+import java.util.regex.Pattern;
+
+/**
+ * @Author ply
+ * @Description 正则表达式测试类
+ * @Date created in 2019/11/26
+ * @ModifiedBy
+ */
+public class Regex {
+    public static void main(String[] args) {
+
+        //将字符串转化为普通字符串，匹配时，不会当做正则表达式处理
+        String quote = Pattern.quote("(");
+    }
+}
