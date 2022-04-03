4b1494e21162414c04552013920cd9136d3a7f38
ply <luoyang_peng@foxmail.com>
2022-04-03 12:05:58 +0800

    gc

---
+++ cloud-server/src/main/java/cn/ply/cloud/cloudserver/distributed/BaseInfo.java
@@ -0,0 +1,19 @@
+package cn.ply.cloud.cloudserver.distributed;
+
+/**
+ * @Author ply
+ * @Description
+ * @Date created in 2022/4/2
+ * @ModifiedBy
+ */
+public class BaseInfo {
+    /**
+     * CAP
+     * consistency：数据一致性
+     *      弱一致性：弱一致性是相对于强一致性而言，它不保证总能得到最新的值；
+     *      最终一致性(eventual consistency)：放宽对时间的要求，在被调完成操作响应后的某个时间点，被调多个节点的数据最终达成一致
+     * availability：可用性
+     * partition-tolerance：分区一致性
+     *
+     */
+}
